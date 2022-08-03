4a884c26e9dfda440c5971d972321e6ee1dc1137
LeunamCrack <82254221+leunamcrack@users.noreply.github.com>
2022-08-02 20:58:54 -0500

    Updated config

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ module.exports = {
     nextLinks: false,
     searchPlaceholder: 'Search',
     prevLinks: false,
-    docsDir: '/docs/main/',
+    docsDir: '/docs/',
     sidebarDepth: 0,
     lastUpdated: false,
     editLinks: false,
@@ -42,63 +42,70 @@ module.exports = {
     nav: [
       {
         text: 'Documentation',
-        link: '/docs/main/',
+        link: '/docs/introduction/welcome.html',
       },
       {
         text: 'Support',
         link: 'https://discord.gg/H72KFXm'
       }
     ],
-    sidebar: [
-      {
-        title: 'WELCOME',   // required
-        path: '/docs/main/',    // optional, link of the title, which should be an absolute path and must exist
-        collapsable: false, // optional, defaults to true
-      },
-      {
-        title: 'CLASS',
-        path: "",
-        collapsable: false,
-        children: [
-          { title: 'Badges', path: '/docs/main/class/badges.md', collapsable: false, children: [] },
-          { title: 'Category', path: '/docs/main/class/category.md', collapsable: false, children: [] },
-          { title: 'CategoryManager', path: '/docs/main/class/categorymanager.md', collapsable: false, children: [] },
-          { title: 'Client', path: '/docs/main/class/client.md', collapsable: false, children: [] },
-          { title: 'ClientUser', path: '/docs/main/class/clientuser.md', collapsable: false, children: [] },
-          { title: 'Group', path: '/docs/main/class/group.md', collapsable: false, children: [] },
-          { title: 'LeaderBoard', path: '/docs/main/class/leaderboard.md', collapsable: false, children: [] },
-          { title: 'Model', path: '/docs/main/class/model.md', collapsable: false, children: [] },
-          { title: 'ModelManager', path: '/docs/main/class/modelmanager.md', collapsable: false, children: [] },
-          { title: 'ResultManager', path: '/docs/main/class/resultmanager.md', collapsable: false, children: [] },
-          { title: 'TTSResult', path: '/docs/main/class/ttsresult.md', collapsable: false, children: [] },
-          { title: 'User', path: '/docs/main/class/user.md', collapsable: false, children: [] }
-        ]
-      },
-      {
-        title: 'TYPEOF',
-        path: "",
-        collapsable: false,
-        children: [
-          { title: 'BadgeInfo', path: '/docs/main/typeof/badgeinfo.md', collapsable: false, children: [] },
-          { title: 'BadgeName', path: '/docs/main/typeof/badgename.md', collapsable: false, children: [] },
-          { title: 'CategoryFeatures', path: '/docs/main/typeof/categoryfeatures.md', collapsable: false, children: [] },
-          { title: 'CategoryResolvable', path: '/docs/main/typeof/categoryresolvable.md', collapsable: false, children: [] },
-          { title: 'CategoryToken', path: '/docs/main/typeof/categorytoken.md', collapsable: false, children: [] },
-          { title: 'ClientEditOptions', path: '/docs/main/typeof/clienteditoptions.md', collapsable: false, children: [] },
-          { title: 'LeaderBoardItem', path: '/docs/main/typeof/leaderboarditem.md', collapsable: false, children: [] },
-          { title: 'LinksData', path: '/docs/main/typeof/linksdata.md', collapsable: false, children: [] },
-          { title: 'ModelFeatures', path: '/docs/main/typeof/modelfeatures.md', collapsable: false, children: [] },
-          { title: 'ModelResolvable', path: '/docs/main/typeof/modelresolvable.md', collapsable: false, children: [] },
-          { title: 'ModelToken', path: '/docs/main/typeof/modeltoken.md', collapsable: false, children: [] },
-          { title: 'QueueInfo', path: '/docs/main/typeof/queueinfo.md', collapsable: false, children: [] },
-          { title: 'ResultResolvable', path: '/docs/main/typeof/resultresolvable.md', collapsable: false, children: [] },
-          { title: 'ResultToken', path: '/docs/main/typeof/resulttoken.md', collapsable: false, children: [] },
-          { title: 'Spectrogram', path: '/docs/main/typeof/spectrogram.md', collapsable: false, children: [] },
-          { title: 'UserToken', path: '/docs/main/typeof/usertoken.md', collapsable: false, children: [] },
-          { title: 'Visibility', path: '/docs/main/typeof/visibility.md', collapsable: false, children: [] },
-        ]
-      }
-    ]
+    sidebar: {
+      "/docs/": [
+        {
+          title: 'GENERAL',  
+          path: "",
+          collapsable: false,
+          children: [
+            { title: 'Welcome', path: '/docs/introduction/welcome.md', collapsable: false, children: [] },
+            { title: 'Getting Started', path: '/docs/introduction/getting-started.md', collapsable: false, children: [] },
+          ]
+        },
+        {
+          title: 'CLASS',
+          path: "",
+          collapsable: false,
+          children: [
+            { title: 'Badges', path: '/docs/class/badges.md', collapsable: false, children: [] },
+            { title: 'Category', path: '/docs/class/category.md', collapsable: false, children: [] },
+            { title: 'CategoryManager', path: '/docs/class/categorymanager.md', collapsable: false, children: [] },
+            { title: 'Client', path: '/docs/class/client.md', collapsable: false, children: [] },
+            { title: 'ClientUser', path: '/docs/class/clientuser.md', collapsable: false, children: [] },
+            { title: 'Group', path: '/docs/class/group.md', collapsable: false, children: [] },
+            { title: 'LeaderBoard', path: '/docs/class/leaderboard.md', collapsable: false, children: [] },
+            { title: 'Model', path: '/docs/class/model.md', collapsable: false, children: [] },
+            { title: 'ModelManager', path: '/docs/class/modelmanager.md', collapsable: false, children: [] },
+            { title: 'ResultManager', path: '/docs/class/resultmanager.md', collapsable: false, children: [] },
+            { title: 'TTSResult', path: '/docs/class/ttsresult.md', collapsable: false, children: [] },
+            { title: 'User', path: '/docs/class/user.md', collapsable: false, children: [] }
+          ]
+        },
+        {
+          title: 'TYPEOF',
+          path: "",
+          collapsable: false,
+          children: [
+            { title: 'BadgeInfo', path: '/docs/typeof/badgeinfo.md', collapsable: false, children: [] },
+            { title: 'BadgeName', path: '/docs/typeof/badgename.md', collapsable: false, children: [] },
+            { title: 'CategoryFeatures', path: '/docs/typeof/categoryfeatures.md', collapsable: false, children: [] },
+            { title: 'CategoryResolvable', path: '/docs/typeof/categoryresolvable.md', collapsable: false, children: [] },
+            { title: 'CategoryToken', path: '/docs/typeof/categorytoken.md', collapsable: false, children: [] },
+            { title: 'ClientEditOptions', path: '/docs/typeof/clienteditoptions.md', collapsable: false, children: [] },
+            { title: 'ClientOptions', path: '/docs/typeof/clientoptions.md', collapsable: false, children: [] },
+            { title: 'LeaderBoardItem', path: '/docs/typeof/leaderboarditem.md', collapsable: false, children: [] },
+            { title: 'LinksData', path: '/docs/typeof/linksdata.md', collapsable: false, children: [] },
+            { title: 'ModelFeatures', path: '/docs/typeof/modelfeatures.md', collapsable: false, children: [] },
+            { title: 'ModelResolvable', path: '/docs/typeof/modelresolvable.md', collapsable: false, children: [] },
+            { title: 'ModelToken', path: '/docs/typeof/modeltoken.md', collapsable: false, children: [] },
+            { title: 'QueueInfo', path: '/docs/typeof/queueinfo.md', collapsable: false, children: [] },
+            { title: 'ResultResolvable', path: '/docs/typeof/resultresolvable.md', collapsable: false, children: [] },
+            { title: 'ResultToken', path: '/docs/typeof/resulttoken.md', collapsable: false, children: [] },
+            { title: 'Spectrogram', path: '/docs/typeof/spectrogram.md', collapsable: false, children: [] },
+            { title: 'UserToken', path: '/docs/typeof/usertoken.md', collapsable: false, children: [] },
+            { title: 'Visibility', path: '/docs/typeof/visibility.md', collapsable: false, children: [] },
+          ]
+        }
+      ]
+    }
   },
 
   /**
